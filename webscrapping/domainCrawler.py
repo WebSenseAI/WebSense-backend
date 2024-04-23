9e06d5464d9f5d59cb384e5f9f2e1739a12b8721
HydraCc <tan.dalkiran@gmail.com>
2024-04-23 22:42:58 +0200

    ci: seperated sitemap logic into a new file

--- webscrapping/domainCrawler.py
+++ webscrapping/domainCrawler.py
@@ -1,11 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
-from urllib.robotparser import RobotFileParser
-import xml.etree.ElementTree as ElementTree
-import xmltodict
-import pandas as pd
-import xml
+from sitemapExtractor import get_robots_results, fetch_all_sitemap_paths, mine_pages_form_sitemap
+
 
 class Crawler:
     """
@@ -35,17 +32,17 @@ class Crawler:
         """
         
         # Get sitemap data from robots.txt
-        self.get_robots_results()
+        robots_results = get_robots_results(self.domain.geturl())
         
         # if no robots or sitemap page exists then BFS
-        if self.robots_sitemaps and not force_BFS:
+        if robots_results and not force_BFS:
             # THERE IS A SITEMAP IN ROBOTS.TXT
             
             # list all the non-INDEX sitemaps
-            self.fetch_all_sitemap_paths()
+            sitemap_paths = fetch_all_sitemap_paths(robots_results)
             
             # Extract URLs from sitemaps
-            self.get_page_urls()
+            self.sites_reached = mine_pages_form_sitemap(sitemap_paths)
         else:
             # Start BFS discovery
             self.BFS_discover()
@@ -107,127 +104,6 @@ class Crawler:
         parsed = urlparse(url)
         return bool(parsed.netloc) and parsed.netloc == self.domain.netloc
 
-    
-    def get_robots_results(self):
-        """
-        Extract the sitemap from robots.txt.
-        Sets robots_sitemaps attr with the extracted sitemaps
-        None if no robots.txt or sitemap.xml exists.
-
-        """
-        url_robots = urljoin(self.domain.geturl(), 'robots.txt')
-        rp = RobotFileParser()
-        rp.set_url(url_robots)
-        rp.read()
-        sitemaps = rp.site_maps()
-        self.robots_sitemaps = sitemaps
-    
-    def fetch_all_sitemap_paths(self):
-        """Fills the sitemap_paths with only non-INDEX sitemaps."""
-        
-        robots_results = self.robots_sitemaps
-        all_paths = []
-        for path in robots_results:
-            path_type = self.get_sitemap_type(path, crucial=True) 
-            print(path, path_type)
-            if path_type == 'INDEX':
-                all_paths += self.expand_sitemap_index(path)
-            elif  path_type == 'SITEMAP':
-                all_paths += [path]
-            else:
-                continue
-
-        self.sitemap_paths = all_paths
-
-    def get_sitemap_type(self, path, crucial= False):
-        """
-        Identifies the type of the sitemap.
-        The type can be either INDEX, SITEMAP or None.
-        If crucial is selected, then time-expensive method is employed
-        regardless of the value of 'expensive' attribute.
-        """
-
-        if self.expensive or crucial:
-            # THE EXPENSIVE WAY
-            xml = self.get_and_turn_xml_to_dict(path)
-            if 'sitemapindex' in xml:
-                return "INDEX"
-            elif 'urlset' in xml:
-                return "SITEMAP"
-            return None
-        else:
-            # THE CHEAP WAY
-            if 'index' in path:
-                return "INDEX"
-            else:
-                return "SITEMAP"
-    
-    def get_page_urls(self):
-        """
-        From the list of non-INDEX sitemaps, extracts the page URLs.
-        """
-
-        for sitemap in self.sitemap_paths:
-            sitemap_dict = self.get_and_turn_xml_to_dict(sitemap)
-            pages = sitemap_dict['urlset']['url']
-            if type(pages) == list:
-                self.sites_reached += [page['loc'] for page in pages]
-            else:
-                self.sites_reached += [pages['loc']]
-
-
-    def get_and_turn_xml_to_dict(self, path):
-        """
-        Gets the xml from a URL and parses it to dict.
-        If the data in URL is not XML-parseable then it returns None
-        
-        Parameters
-        ----------
-        path : str
-            The URL at which the XML is located
-        
-        Returns
-        -------
-        dict or None
-        """
-        sitemap_xml = requests.get(path).text
-        try:
-            sitemap_dict = xmltodict.parse(sitemap_xml)
-            return sitemap_dict
-        except xml.parsers.expat.ExpatError:
-            print(f"ERROR OCCURED TRYING TO PARSE {path}")
-            return None
-        
-    def expand_sitemap_index(self, index_path):
-        """
-        Given a path of a sitemap INDEX, extracts the actual SITEMAPS.
-        if the INDEX contains other INDEX, then recursively extracts them too.
-
-        Parameters
-        ----------
-        path : str
-            The path of the INDEX
-        
-        Returns
-        -------
-        list
-            List of non-INDEX sitemaps.
-        """
-        sitemap_content = self.get_and_turn_xml_to_dict(index_path)
-        if sitemap_content is None:
-            return [] 
-        sm_dicts = sitemap_content['sitemapindex']['sitemap']
-        sm_values = [sm['loc'] for sm in sm_dicts]
-        expansion = []
-        for sitemap in sm_values:
-            if self.get_sitemap_type(sitemap) == 'INDEX':
-                print(f'expanding {sitemap}')
-                expansion += self.expand_sitemap_index(sitemap)
-            else:
-                print(f'added {sitemap}')
-                expansion += [sitemap]
-        return expansion
-    
     def save(self, path=None):
         """Dev Tool : saves the crawled sites into a .txt file"""
         if path is None:
