9d18a6d65ae189f64b0b87066506f2c370626133
Tan Dalkıran <tan.dalkiran@gmail.com>
2024-11-12 00:36:44 +0100

    feat: added per country statistics

--- app/services/database/chat_db.py
+++ app/services/database/chat_db.py
@@ -12,11 +12,21 @@ def insert_new_question(bot_id:str, question: str, response: str, metadata: dict
 
 
 
-def get_questions_and_count(bot_id:str):
-    action = supabase.schema('public').rpc('chat_get_conversations_by_bot', {'botid': bot_id}).execute()
-    return action.data, len(action.data)
+def get_questions_and_count(bot_id:str, access_token:str):
+    action = supabase.schema('public').rpc('chat_get_conversations_by_bot', {'botid': bot_id})
+    action.headers['Authorization'] = 'Bearer ' + access_token
+    response = action.execute()
+    return response.data, len(response.data)
 
-def get_unique_users(bot_id:str):
-    action = supabase.schema('public').rpc('chat_get_unique_user_count', {'botid': bot_id}).execute()
-    return action.data, len(action.data)
-    
+def get_unique_users(bot_id:str, access_token:str):
+    action = supabase.schema('public').rpc('chat_get_unique_user_count', {'botid': bot_id})
+    action.headers['Authorization'] = 'Bearer ' + access_token
+    response = action.execute()
+    return response.data, len(response.data)
+
+
+def get_countries(bot_id:str, access_token:str):
+    action = supabase.schema('public').rpc('chat_get_country_stats', {'botid': bot_id})
+    action.headers['Authorization'] = 'Bearer ' + access_token
+    response = action.execute()
+    return response.data, len(response.data)
